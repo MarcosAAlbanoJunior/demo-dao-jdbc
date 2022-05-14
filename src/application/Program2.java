31009e1d860a8a66adb10ca958ea0c8e98a7b3fe
Marcos Albano <marcosaalbanojunior@gmail.com>
2022-05-14 05:30:23 -0300

    DepartmentDao implementation and test

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,5 +1,8 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
 import model.entities.Department;
@@ -11,16 +14,38 @@ public class Program2 {
 		
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 		
-		System.out.println("\n=== TEST 1: seller insert=====");
+		System.out.println("\n=== TEST 1: Department insert=====");
 		Department newDepartment = new Department(null, "Music");
 		departmentDao.insert(newDepartment);
 		System.out.println("Inserted! New id = " + newDepartment.getId());
 		
-		System.out.println("\n=== TEST 2: seller Delete=====");
+		System.out.println("\n=== TEST 2: Department Delete=====");
 		int id = 7;
 		departmentDao.deleteById(id);
 		System.out.println("Deleted done!");
+		
+		System.out.println("\n=== TEST 3: Department findById=====");
+		Department dep = departmentDao.findById(1);
+		System.out.println(dep);
+		
+		System.out.println("\n=== TEST 4: update =======");
+		Department dep2 = departmentDao.findById(1);
+		dep2.setName("Food");
+		departmentDao.update(dep2);
+		System.out.println("Update completed");
+		
+		System.out.println("\n=== TEST 5: findAll =======");
+		
+		List <Department> depp = new ArrayList<>();
+		depp = departmentDao.findAll();
+		for (Department x: depp) {
+			System.out.println(x);
+		}
+		
 
+		
+		
+		
 	}
 
 }
